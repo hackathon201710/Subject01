8ffad62ff24b1ef36401edec83d3cde3f8159154
Malin Brolien <malin.brolien@polisen.se>
2017-10-11 11:13:53 +0200

    map and position

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,43 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { Geolocation } from '@ionic-native/geolocation';
+
+declare var google;
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+  @ViewChild('map') mapElement: ElementRef;
+  map: any;
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController, public geolocation: Geolocation) {
 
   }
+
+  ionViewDidLoad(){
+    this.loadMap();
+  }
+
+  loadMap(){
+
+    this.geolocation.getCurrentPosition().then((position) =>{
+    
+      let latLng = new google.maps.LatLng(position.coords.latitude,position.coords.longitude);
+
+       let mapOptions = {
+        center: latLng, 
+        zoom: 15
+      }
+
+      this.map = new google.maps.Map(this.mapElement.nativeElement, mapOptions);
+
+    }, (err) =>{
+      console.log(err);
+    })
+
+  }
+
 
 }
