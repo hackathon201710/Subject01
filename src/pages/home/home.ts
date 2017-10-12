b9662507eb190f8a73901d6b159cf80d109ebbf3
Mauricio Bustos <mauricio@Mauricios-MBP.lan>
2017-10-12 10:11:20 +0200

    Working com between client and cloud

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -24,14 +24,13 @@ export class HomePage {
   unpairedDevices: any;
   pairedDevices: any;
   gettingDevices: Boolean;
-  private headers = new Headers({'Content-Type': 'application/json'});
-
+  private headers = new Headers({ 'Content-Type': 'application/json' });
 
   constructor(private bluetoothSerial: BluetoothSerial, public navCtrl: NavController, public geolocation: Geolocation, private alertCtrl: AlertController) {
     bluetoothSerial.enable();
   }
   ionViewDidLoad() {
-   // this.loadMap();
+    // this.loadMap();
   }
 
   startScanning() {
@@ -49,20 +48,20 @@ export class HomePage {
         console.log('his.bluetoothSerial.list error' + err);
       })
 
-/*     console.log('Before startScanning');
-    this.bluetoothSerial.discoverUnpaired().then((success) => {
-      console.log('Inside startScanning');
-      this.unpairedDevices = success;
-      this.gettingDevices = false;
-      success.forEach(element => {
-        console.log('Inside startScanning success');
-        alert(element.name);
-      });
-    },
-      (err) => {
-        console.log('startScanning error');
-        console.log(err);
-      }) */
+    /*     console.log('Before startScanning');
+        this.bluetoothSerial.discoverUnpaired().then((success) => {
+          console.log('Inside startScanning');
+          this.unpairedDevices = success;
+          this.gettingDevices = false;
+          success.forEach(element => {
+            console.log('Inside startScanning success');
+            alert(element.name);
+          });
+        },
+          (err) => {
+            console.log('startScanning error');
+            console.log(err);
+          }) */
 
     this.bluetoothSerial.list().then((success) => {
       this.pairedDevices = success;
@@ -121,70 +120,84 @@ export class HomePage {
   }
 
 
-  emergencyButtonPressed(){
+  emergencyButtonPressed() {
     clearInterval(intervalFunction);
     intervalFunction = setInterval(() => {
-      this.geolocation.getCurrentPosition().then((position) =>{
-        if(!this.map){
+      this.geolocation.getCurrentPosition().then((position) => {
+        if (!this.map) {
           this.initMap(position.coords.latitude, position.coords.longitude);
         }
-        else{
+        else {
           this.updateMap(position.coords.latitude, position.coords.longitude);
         }
-        console.log("skicka pos: " + position.coords.latitude + " " +  position.coords.longitude)
+        console.log("skicka pos: " + position.coords.latitude + " " + position.coords.longitude)
 
         this.postLocation(position.coords.latitude, position.coords.longitude);
 
-      }, (err) =>{
-      console.log(err);
-    })
-  }, 2000);
+      }, (err) => {
+        console.log(err);
+      })
+    }, 2000);
   }
 
-initMap(lat, long){
+  initMap(lat, long) {
     let latLng = new google.maps.LatLng(lat, long);
 
-       let mapOptions = {
-        center: latLng,
-        zoom: 15
-      }
+    let mapOptions = {
+      center: latLng,
+      zoom: 15
+    }
 
-      this.map = new google.maps.Map(this.mapElement.nativeElement, mapOptions);
+    this.map = new google.maps.Map(this.mapElement.nativeElement, mapOptions);
 
-      new google.maps.Marker({
-        map: this.map,
-        animation: google.maps.Animation.DROP,
-        position: this.map.getCenter()
-      });
-}
-
-  updateMap(lat, long){
-      new google.maps.Marker({
-        map: this.map,
-        animation: google.maps.Animation.DROP,
-        position: this.map.getCenter()
-      });
+    new google.maps.Marker({
+      map: this.map,
+      animation: google.maps.Animation.DROP,
+      position: this.map.getCenter()
+    });
   }
 
-  postLocation(lat, long){
-//'http://localhost:3000/location'
+  updateMap(lat, long) {
+    new google.maps.Marker({
+      map: this.map,
+      animation: google.maps.Animation.DROP,
+      position: this.map.getCenter()
+    });
+  }
 
- $fh.cloud({
-          path:'location',
-          method: "POST",
-           data: {
-           longitude: long,
+  postLocation(lat, long) {
+    //'http://localhost:3000/location'
+
+    /*     $fh.cloud({
+          "path": '/location',
+          "method": "POST",
+          data: {
+            longitude: long,
             latitude: lat
-        }
-      }, function(res) {
+          }
+        }, function (res) {
           console.log('success: ', res);
-      }, function(err) {
+        }, function (err) {
           console.log("error");
-      });
+        }); */
 
-  /*this.http.post('https://polisen-raukdn4hwmk5hf54qybpwwkq-dev.mbaas1.tom.redhatmobile.com/location', JSON.stringify({lat: latitude, long: longitude}), {headers: this.headers}).subscribe(res => {
-        console.log("successs");
-      });*/
+    $fh.cloud({
+      path: 'hello', //only the path part of the url, the host will be added automatically
+      method: 'POST', //all other HTTP methods are supported as well. For example, HEAD, DELETE, OPTIONS
+      contentType: 'application/json',
+      data: { longitude: long, latitude: lat }, //data to send to the server
+      timeout: 25000 // timeout value specified in milliseconds. Default: 60000 (60s)
+    }, function (res) {
+      // Cloud call was successful. Alert the response
+      //alert('Got response from cloud:' + JSON.stringify(res));
+    }, function (msg, err) {
+      // An error occurred during the cloud call. Alert some debugging information
+      //alert('Cloud call failed with error message:' + msg + '. Error properties:' + JSON.stringify(err));
+    });
 
-}
+    /*this.http.post('https://polisen-raukdn4hwmk5hf54qybpwwkq-dev.mbaas1.tom.redhatmobile.com/location', JSON.stringify({lat: latitude, long: longitude}), {headers: this.headers}).subscribe(res => {
+          console.log("successs");
+        });*/
+
+  }
 }
