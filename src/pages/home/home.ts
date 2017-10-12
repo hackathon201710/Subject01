858a062b6a83974337038b4154c033ff9b0d7fdb
hackathon201710 <hackathon201710@mailinator.com>
2017-10-12 12:13:03 +0200

    Added write function

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -23,101 +23,45 @@ export class HomePage {
 
   unpairedDevices: any;
   pairedDevices: any;
+  pairedDeviceName: any;
+  pairedDeviceAddress: any;
   gettingDevices: Boolean;
   private headers = new Headers({ 'Content-Type': 'application/json' });
 
   constructor(private bluetoothSerial: BluetoothSerial, public navCtrl: NavController, public geolocation: Geolocation, private alertCtrl: AlertController) {
     bluetoothSerial.enable();
+
+      this.bluetoothSerial.list().then((success) => {
+              console.log('Inside List ' + success);
+              this.pairedDevices = success;
+              for (let i = 0; i < this.pairedDevices.length; i++) {
+                  console.log(this.pairedDevices[i].name + " : " + this.pairedDevices[i].address);
+                  if (this.pairedDevices[i].name == "Andreass MacBook Pro") {
+                      this.pairedDeviceName = this.pairedDevices[i].name;
+                      this.pairedDeviceAddress = this.pairedDevices[i].address;
+
+                      console.log(this.pairedDeviceName + " : " + this.pairedDeviceAddress);
+
+                      console.log("Trying to connect");
+                      this.bluetoothSerial.connect(this.pairedDeviceAddress).subscribe(this.success, this.fail);
+
+                  }
+              }
+          },
+          (err) => {
+              console.log('his.bluetoothSerial.list error' + err);
+          });
   }
   ionViewDidLoad() {
     // this.loadMap();
   }
 
-  startScanning() {
-    console.log('Enter startScanning');
-    this.pairedDevices = null;
-    this.unpairedDevices = null;
-    this.gettingDevices = true;
-    console.log('Before list');
-    this.bluetoothSerial.list().then((success) => {
-      console.log('Inside List ' + success);
-      this.pairedDevices = success;
-      debugger;
-    },
-      (err) => {
-        console.log('his.bluetoothSerial.list error' + err);
-      })
-
-    /*     console.log('Before startScanning');
-        this.bluetoothSerial.discoverUnpaired().then((success) => {
-          console.log('Inside startScanning');
-          this.unpairedDevices = success;
-          this.gettingDevices = false;
-          success.forEach(element => {
-            console.log('Inside startScanning success');
-            alert(element.name);
-          });
-        },
-          (err) => {
-            console.log('startScanning error');
-            console.log(err);
-          }) */
-
-    this.bluetoothSerial.list().then((success) => {
-      this.pairedDevices = success;
-    },
-      (err) => {
-
-      })
+  write() {
+    this.bluetoothSerial.write("hello, world");
   }
+
   success = (data) => alert(data);
   fail = (error) => alert(error);
-
-  selectDevice(address: any) {
-    let alert = this.alertCtrl.create({
-      title: 'Connect',
-      message: 'Do you want to connect with?',
-      buttons: [
-        {
-          text: 'Cancel',
-          role: 'cancel',
-          handler: () => {
-            console.log('Cancel clicked');
-          }
-        },
-        {
-          text: 'Connect',
-          handler: () => {
-            this.bluetoothSerial.connect(address).subscribe(this.success, this.fail);
-          }
-        }
-      ]
-    });
-    alert.present();
-  }
-
-  disconnect() {
-    let alert = this.alertCtrl.create({
-      title: 'Disconnect?',
-      message: 'Do you want to Disconnect?',
-      buttons: [
-        {
-          text: 'Cancel',
-          role: 'cancel',
-          handler: () => {
-            console.log('Cancel clicked');
-          }
-        },
-        {
-          text: 'Disconnect',
-          handler: () => {
-            this.bluetoothSerial.disconnect();
-          }
-        }
-      ]
-    });
-    alert.present();
-  }
 
 
   emergencyButtonPressed() {
